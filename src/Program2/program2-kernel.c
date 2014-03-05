c33769a14d88bb175fc2394a8173b626b32fb843
Peter Henderson <Breakend@web008042.wireless.mcgill.ca>
2014-03-05 14:12:14 -0500

    More attempts at loading 2 programs with no success :(

--- src/Program2/program2-kernel.c
+++ src/Program2/program2-kernel.c
@@ -1,21 +1,42 @@
-#include <avr/io.h>
-#include <util/delay.h>
+// #include <avr/io.h>
+// #include <util/delay.h>
  
 int main (void)
 {
  
   /* Note this works with ARM so may need to change */
 
-  /* 8-bit address*/
-  unsigned int address = 0x8002; 
+  // /* 8-bit address*/
+  // unsigned int address = 0xb000; 
 
-  //pointer to the address
-  typedef void (*funcPtr)(void); 
+  // //pointer to the address
+  // typedef void (*funcPtr)(void); 
 
-  funcPtr ptr = (funcPtr)address; 
+  // funcPtr ptr = (funcPtr)address; 
 
-  /* Essentially a jump to that address */
-  ptr(); 
+  // /* Essentially a jump to that address */
+  // ptr(); 
+
+  //Attempt #2
+  /* No return saves from pointing to return address*/
+  // typedef void (*AppPtr_t)(void) __attribute__ ((noreturn)); 
+
+  // AppPtr_t AppStartPtr = (AppPtr_t)0x00b0; 
+  // AppStartPtr();
+
+//Attempt #3
+// ((void (*)())0x00b0)();
+
+  //Attempt #5
+  // void (*fptr)(void);
+ 
+  //  fptr = (void (*)(void))0x00c0;
+  //  fptr();
+
+  asm volatile(
+    "jmp 0x00c0"
+    );
+
 
  return 0;
 }
